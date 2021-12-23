648d7ee03d9314f0699034e69b97d856463aa098
Nguyễn Hoàng Thiên Ân <57244622+ThienAn1010@users.noreply.github.com>
2021-12-23 16:32:20 +0700

    add team details

--- assignment2_group25/assignment2_group25.cpp
+++ assignment2_group25/assignment2_group25.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+using namespace std;
+
+void team_detail() {
+    cout << "" << endl;
+    cout << "ASIGNMENT 2 - GROUP 25" << endl;
+    cout << "s3825455, s3825455@rmit.edu.vn, Thien An, Nguyen Hoang" << endl;
+    cout << "s3893635, s3893635@rmit.edu.vn, Minh Quan, Che" << endl;
+    cout << "s3875336, s3875336@rmit.edu.vn, Nam Vinh, Nguyen" << endl;
+    cout << "s3817907, s3817907@rmit.edu.vn, Anh Tuan, Nguyen" << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    team_detail();
 }
 
